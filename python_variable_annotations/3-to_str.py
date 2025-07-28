c9cd7725253ea4340f82040fdcccf65ea5f9a9cf
Annece2935 <anne-cecile.colleter@live.fr>
2025-07-28 07:54:17 +0200

    add task 3 :Write a type-annotated function to_str that takes a float n as argument and returns the string representation of the float.

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Module de conversion.
+
+Ce module contient une fonction `to_str` qui convertit un nombre flottant
+en chaîne de caractères.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Convertit un nombre flottant en chaîne de caractères.
+
+    Args:
+        n (float): Le nombre flottant à convertir.
+
+    Returns:
+        str: La représentation en chaîne de caractères de n.
+    """
+    return str(n)
