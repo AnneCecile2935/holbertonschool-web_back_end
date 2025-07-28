66c3abda02b7541238f26adaefefbb088bcf92a6
Annece2935 <anne-cecile.colleter@live.fr>
2025-07-28 07:48:49 +0200

    add task 2 :Write a type-annotated function floor which takes a float n as argument and returns the floor of the float.

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Module pour les opérations mathématiques simples.
+
+Ce module fournit une fonction `floor` qui calcule le plancher
+(définition mathématique : plus grand entier inférieur ou égal)
+d'un nombre flottant donné.
+"""
+
+
+import math
+
+
+def floor(n: float) -> float:
+    """
+    Retourne le plancher (floor) de la valeur flottante n.
+
+    Args:
+        n (float): Le nombre flottant dont on veut le plancher.
+
+    Returns:
+        int: Le plus grand entier inférieur ou égal à n.
+    """
+    return math.floor(n)
