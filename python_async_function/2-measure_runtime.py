06714e60ee794b2596863fd892264cbb24175a86
Annece2935 <anne-cecile.colleter@live.fr>
2025-07-28 10:35:57 +0200

    add task 2 : From the previous file, import wait_n into 2-measure_runtime.py.
    Create a measure_time function with integers n and max_delay as arguments that measures the total execution time for wait_n(n, max_delay), and returns total_time / n. Your function should return a float.
    
    Use the time module to measure an approximate elapsed time.

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,1 +1,40 @@
 #!/usr/bin/env python3
+"""
+Module de mesure de performance asynchrone.
+
+Ce module utilise la fonction `wait_n` pour exécuter plusieurs appels
+asynchrones et calcule le temps moyen d'exécution par appel.
+"""
+
+from concurrent_coroutines import wait_n
+import asyncio
+import time
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """
+    Mesure le temps moyen d'exécution d'un appel à wait_n.
+
+    Args:
+        n (int): Le nombre de fois que la coroutine `wait_random`
+        sera lancée via `wait_n`.
+        max_delay (int): Le délai maximum (en secondes) pour chaque
+        attente aléatoire.
+
+    Returns:
+        float: Le temps moyen d'exécution par appel (en secondes).
+    """
+    # On enregistre le temps de départ avec haute précision
+    start = time.perf_counter()
+
+    # Exécution de la coroutine `wait_n` de façon synchrone dans l'event loop
+    asyncio.run(wait_n(n, max_delay))
+
+    # Temps après exécution
+    end = time.perf_counter()
+
+    # Temps total écoulé
+    total_time = end - start
+
+    # Temps moyen par appel
+    return total_time / n
