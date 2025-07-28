54ccd4e3da631e40363d9fb766127d7cfaad8b01
Annece2935 <anne-cecile.colleter@live.fr>
2025-07-28 07:59:46 +0200

    add task 4 Define and annotate the following variables with the specified values:

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Module d'exemple montrant la déclaration de variables avec annotations de type.
+
+Variables déclarées :
+- a (int) : un entier.
+- pi (float) : un nombre à virgule flottante.
+- i_understand_annotations (bool) : un booléen.
+- school (str) : une chaîne de caractères.
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
