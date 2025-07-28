160724cf2267fbd92dda98e652e8236f21a32fe3
Annece2935 <anne-cecile.colleter@live.fr>
2025-07-28 07:27:50 +0200

    add task 0. Write a type-annotated function add that takes a float a and a float b as arguments and returns their sum as a float. with documentation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
-def add(a: float, b:float) -> float:
-    return a + b
+"""
+Module de calcul simple.
 
+Ce module contient une fonction `add` qui permet de calculer la somme
+de deux nombres à virgule flottante (float). Il illustre l'utilisation
+des annotations de type et des docstrings en Python.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Calcule la somme de deux nombres flottants.
+
+    Args:
+        a (float): Le premier nombre.
+        b (float): Le deuxième nombre.
+
+    Returns:
+        float: La somme de a et b.
+    """
+    return a + b
