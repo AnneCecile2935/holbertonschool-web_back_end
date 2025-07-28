2e703f600c31e266221c2a42b125b9cfe42849c4
Annece2935 <anne-cecile.colleter@live.fr>
2025-07-28 10:28:46 +0200

    task 1 : Import wait_random from the previous python file that you’ve written and write an async routine called wait_n that takes in 2 int arguments (in this order): n and max_delay. You will spawn wait_random n times with the specified max_delay.
    wait_n should return the list of all the delays (float values). The list of the delays should be in ascending order without using sort() because of concurrency.

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,1 +1,27 @@
 #!/usr/bin/env python3
+"""
+Module asynchrone qui lance plusieurs appels à wait_random et
+retourne les délais obtenus, triés par ordre croissant.
+"""
+
+
+from basic_async_syntax import wait_random
+from typing import List
+import asyncio
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Lance wait_random n fois de manière asynchrone et retourne
+    la liste des délais obtenus, triée par ordre croissant.
+
+    Args:
+        n (int): nombre d’appels à wait_random.
+        max_delay (int): valeur max pour chaque délai aléatoire.
+
+    Returns:
+        List[float]: liste des délais, en ordre croissant.
+    """
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return sorted(delays)
