72c82752be8bfd1462a3c575d3082c05a40b351e
Annece2935 <anne-cecile.colleter@live.fr>
2025-07-28 08:20:45 +0200

    task 6 : Write a type-annotated function sum_mixed_list which takes a list mxd_lst of integers and floats and returns their sum as a float.

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Module de calcul sur des listes de nombres mixtes.
+
+Ce module contient une fonction `sum_mixed_list` qui prend en
+entrée une liste composée d'entiers (`int`) et de nombres à virgule
+flottante (`float`), et retourne leur somme en tant que `float`.
+"""
+
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+    """
+    Calcule la somme des éléments d'une liste contenant des floats et des
+    entiers.
+
+    Args:
+        mxd_lst (List[Union[float, int]]): Une liste contenant des nombres
+                                           entiers et/ou flottants.
+
+    Returns:
+        float: La somme des éléments de la liste, en tant que nombre flottant.
+    """
+    return sum(mxd_lst)
