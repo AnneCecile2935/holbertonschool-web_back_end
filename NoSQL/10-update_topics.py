5786f67c5873090f18d3edde83ec2401e4da3342
Annece2935 <anne-cecile.colleter@live.fr>
2025-08-13 07:39:40 +0200

    feat: add update_topics function to update the topics of a school document by name in MongoDB

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Module pour mettre à jour la liste des topics d'un document école
+dans une collection MongoDB
+en fonction du nom de l'école.
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Met à jour le champ 'topics' du document dont le nom est `name`
+    dans la collection mongo_collection.
+
+    Args:
+        mongo_collection (pymongo.collection.Collection):
+        La collection MongoDB.
+        name (str): Nom de l’école à mettre à jour.
+        topics (list of str): Nouvelle liste des topics.
+
+    Returns:
+        pymongo.results.UpdateResult: Résultat de l’opération de mise à jour.
+    """
+    filter = {"name": name}
+    new_values = {"$set": {"topics": topics}}
+    result = mongo_collection.update_one{filter, new_values}
+    return result
