36338c1c5cd022af6914f3b2e7453a1996025b54
Annece2935 <anne-cecile.colleter@live.fr>
2025-08-18 10:59:49 +0200

    update files config

--- ES6_basic/eslint.config.js
+++ ES6_basic/eslint.config.js
@@ -1,24 +1,35 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import { defineConfig } from 'eslint/config';
-
-export default defineConfig([
-  {
-    files: ['**/*.{js,mjs,cjs}'],
-    plugins: { js },
-    extends: ['js/recommended'],
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-        ...globals.jest, // globals pour test Jest : test, expect, etc.
-      },
-    },
-    rules: {
-      'no-var': 'error',
-      'prefer-const': 'error',
-      'semi': ['error', 'always'],
-      'quotes': ['error', 'single'],
-    },
+module.exports = {
+  env: {
+    browser: false,
+    es6: true,
+    jest: true,
   },
-]);
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+  },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
+};
