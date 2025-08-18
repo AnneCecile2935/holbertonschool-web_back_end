3fe23fd871b47f86984d02443c2d34e75b35ba08
Annece2935 <anne-cecile.colleter@live.fr>
2025-08-18 07:51:56 +0200

    update with tests task 0 and 1

--- ES6_basic/0-constant.test.js
+++ ES6_basic/0-constant.test.js
@@ -1,5 +1,14 @@
-import { myFunction } from './0-module.js';
+import {  taskFirst, taskNext} from './0-constants.js';
 
-test('myFunction should return the correct string', () => {
-  expect(myFunction()).toBe("I prefer const when I can. But sometimes let is okay");
+test('taskFirst should return the correct string', () => {
+  expect(taskFirst()).toBe('I prefer const when I can.');
+});
+
+test('taskNext should return the correct string', () => {
+  expect(taskNext()).toBe('But sometimes let is okay');
+});
+
+test('combined output', () => {
+  const combined = `${taskFirst()} ${taskNext()}`;
+  expect(combined).toBe('I prefer const when I can. But sometimes let is okay');
 });
