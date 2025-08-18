b49816f03b0c2a394e66e96f14c9d2e9d653ca65
Annece2935 <anne-cecile.colleter@live.fr>
2025-08-18 08:52:03 +0200

    refactor: convert addNeighborhood to arrow function
    Rewrote the addNeighborhood method using ES6 arrow syntax to inherit
    `this` directly, removing the need for a separate `self` variable.

--- ES6_basic/1-block-scoped.test.js
+++
@@ -1,6 +0,0 @@
-import taskBlock from './1-block-scoped.js';
-
-test('taskBlock returns correct values', () => {
-  expect(taskBlock(true)).toEqual([false, true]);
-  expect(taskBlock(false)).toEqual([false, true]);
-});
