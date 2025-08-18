ccfcb4e3235c6a041671512712c64885d5a6a4fa
Annece2935 <anne-cecile.colleter@live.fr>
2025-08-18 07:39:02 +0200

    update task 1

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    let task = true;   // nouvelle variable locale
-    let task2 = false; // nouvelle variable locale
+    const task = true;
+    const task2 = false;
   }
 
   return [task, task2];
