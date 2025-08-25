dbfa67c97263ee4ffa0b126efe95ec209c16ecdb
Annece2935 <anne-cecile.colleter@live.fr>
2025-08-25 12:00:41 +0200

    feat: add getListStudents function
    - returns an array of student objects with id, firstName, and location
    - includes Guillaume, James, and Serena as specified

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,20 @@
+function getListStudents(){
+	const students = [
+    {
+		id: 1,
+		fisrtName: "Guillaume",
+		location: "San Francisco"
+	},
+  {
+    id: 2,
+		fisrtName: "James",
+		location: "Columbia"
+	},
+  {
+    id: 5,
+		fisrtName: "Serena",
+		location: "San Francisco"
+  }
+];
+	return students;
+}
