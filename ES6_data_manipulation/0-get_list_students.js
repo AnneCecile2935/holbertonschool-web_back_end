92a25feb983b51c91b7e1a2e1ca6f43e529c8f23
Annece2935 <anne-cecile.colleter@live.fr>
2025-08-25 12:05:53 +0200

    update files

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,20 +1,20 @@
-function getListStudents(){
-	const students = [
+function getListStudents() {
+  const students = [
     {
-		id: 1,
-		fisrtName: "Guillaume",
-		location: "San Francisco"
-	},
-  {
-    id: 2,
-		fisrtName: "James",
-		location: "Columbia"
-	},
-  {
-    id: 5,
-		fisrtName: "Serena",
-		location: "San Francisco"
-  }
-];
-	return students;
+      id: 1,
+      fisrtName: 'Guillaume',
+      location: 'San Francisco',
+    },
+    {
+      id: 2,
+      fisrtName: 'James',
+      location: 'Columbia',
+    },
+    {
+      id: 5,
+      fisrtName: 'Serena',
+      location: 'San Francisco',
+    },
+  ];
+  return students;
 }
