b955d29bb5bfc244cd7a460b09d9dce23023f029
Annece2935 <anne-cecile.colleter@live.fr>
2025-08-26 21:26:48 +0200

    update task 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,10 @@
 export default function cleanSet(set, startString) {
-    if (startString === '') return '';
-    let result = [];
-    for (const item of set) {
-        if (item.startsWith(startString)) {
-            result.push(item.slice(startString.length));
-        }
+  if (startString === '') return '';
+  const result = [];
+  for (const item of set) {
+    if (item.startsWith(startString)) {
+      result.push(item.slice(startString.length));
     }
-    return result.join('-');
+  }
+  return result.join('-');
 }
