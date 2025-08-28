534c613671f11124c61e9558009f334ea9f2c6f2
Annece2935 <anne-cecile.colleter@live.fr>
2025-08-28 14:27:28 +0200

    good code for use in real

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,9 +3,8 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => {
-      values[1].value = `Error: ${values[1].reason.message}`;
-      delete values[1].reason;
-      return values;
-  });
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    })));
 }
