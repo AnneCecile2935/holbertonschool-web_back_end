b19cbedc35d7f12c62caff816541aa36fd119dbb
Annece2935 <anne-cecile.colleter@live.fr>
2025-08-28 14:25:54 +0200

    test

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,8 +3,9 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    })));
+    .then((values) => {
+      values[1].value = `Error: ${values[1].reason.message}`;
+      delete values[1].reason;
+      return values;
+  });
 }
