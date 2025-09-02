bcca49c255efa1b7e5c3cb00d9aa006a8f52887f
Annece2935 <anne-cecile.colleter@live.fr>
2025-09-02 14:23:30 +0200

    with end task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,10 +2,10 @@ process.stdin.setEncoding('utf-8');
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
-	const name = process.stdin.read();
-	if (name) process.stdout.write(`Your name is: ${name}`);
+  const name = process.stdin.read();
+  if (name) process.stdout.write(`Your name is: ${name}`);
 });
 
-process.stdin.on('close', () => {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
