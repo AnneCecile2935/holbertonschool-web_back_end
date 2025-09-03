8b1c279c252e79827657648b962eff158f370e65
Annece2935 <anne-cecile.colleter@live.fr>
2025-09-03 08:34:51 +0200

    feat: add Express server with /students endpoint
    - Create Express server listening on port 1245
    - Add GET routes for '/' and '/students'
    - Reuse countStudents logic from 3-read_file_async.js
    - Handle CSV file argument and errors
    - Tested with curl for both endpoints"

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.type('text/plain');
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  res.type('text/plain');
+  const databaseFile = process.argv[2];
+
+  if (!databaseFile) {
+    res.status(400).send('Please provide the database file path as an argument.\n');
+    return; // Important : arrête l'exécution ici
+  }
+
+  countStudents(databaseFile)
+    .then((output) => {
+      res.send(`This is the list of our students\n${output}`);
+    })
+    .catch((error) => {
+      res.status(500).send(error.message);
+    });
+});
+
+app.listen(1245, () => {
+  console.log('Server running at http://localhost:1245/');
+});
+
+module.exports = app;
